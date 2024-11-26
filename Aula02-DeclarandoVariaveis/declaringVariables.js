1c2a44b0c4343fac73a4654c2fbb1e80d1f9d179
vitoriamillnitz <vitoriamillnitz@gmail.com>
2024-11-26 19:24:06 -0300

    clonando repositorio

---
+++ Aula02-DeclarandoVariaveis/declaringVariables.js
@@ -0,0 +1,15 @@
+// var let const
+
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("primeiroValor + segundoValor: ", primeiroValor + segundoValor);
+const valorTotal = primeiroValor + segundoValor;
+console.log("valorTotal: ", valorTotal);
+
+let terceiroValor = 50;
+console.log("terceiroValor: ", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
+
