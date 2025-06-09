7143d0c46bcd7e75772e0cc07648280dc38c1547
vitoriamillnitz <vitoriamillnitz@gmail.com>
2025-06-09 17:28:53 -0300

    aula06

---
+++ Aula06-Arrays/arrays.js
@@ -0,0 +1,14 @@
+const listaDeConvidados = ["Matheus", "Amanda", "Paula", "Thiago"];
+
+console.log("Vitória fez aniversário e convidou:", listaDeConvidados);
+console.log("Quantos convidados?", listaDeConvidados.length);
+
+console.log("Convidado numero 1:", listaDeConvidados[0]);
+console.log("Convidado numero 4:", listaDeConvidados[listaDeConvidados.length - 1]);
+
+console.log("Amanda está em qual índice do nosso array?", listaDeConvidados.indexOf("Amanda"));
+console.log("Amanda está em qual número da lista?", listaDeConvidados.indexOf("Amanda") + 1);
+
+const arrayQualquer = ["Vitoria", true, 24, {valor1: 10, valor2: 20}];
+console.log(arrayQualquer)
+
