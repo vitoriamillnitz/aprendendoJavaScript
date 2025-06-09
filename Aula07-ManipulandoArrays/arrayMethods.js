cb9b94936e8d33eddb7a78002a69f4738a16d66f
vitoriamillnitz <vitoriamillnitz@gmail.com>
2025-06-09 18:58:07 -0300

    Manipulando Arrays

---
+++ Aula07-ManipulandoArrays/arrayMethods.js
@@ -0,0 +1,12 @@
+const listaDeConvidados = ["Matheus", "Izabelli", "Amanda", "Paula"];
+
+// Push / Pop / Shift / Unshift
+
+listaDeConvidados.push("Leonardo");
+console.log(listaDeConvidados);
+
+listaDeConvidados.pop(); //remove o último elemento da lista
+console.log(listaDeConvidados);
+
+listaDeConvidados.pop(2);
+console.log(listaDeConvidados);
